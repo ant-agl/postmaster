e650db28660d3b61183fb2fa7ba8cdae813dd916
EldarTem <temirov.e.i.21@gmail.com>
2023-03-05 16:34:35 +0300

    Колесо фортуны

--- postmasterPHP/headers.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Origin,Content-Type,Accept,X-Requested-With');
--- reg/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'db_name' => 'port25_toolbox',
-    'db_host' => 'localhost',
-    'db_user' => 'port25_toolbox',
-    'db_pass' => 'wN8oN3jQ1w^t9',
-];
